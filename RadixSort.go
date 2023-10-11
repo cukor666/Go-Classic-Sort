4c344e915ebb02513a2d3047666684d0da421955
CukorZhong <2450808861@qq.com>
2023-10-11 13:41:46 +0800

    添加并修改基数排序和主函数测试

--- RadixSort.go
+++ RadixSort.go
@@ -1,5 +1,22 @@
 package main
 
-func RadixSort[T ~uint | ~uint64 | ~uint32 | ~uint8](arr []T) {
+import "math"
 
+func RadixSort(arr []int) {
+	temp := make([][]int, 10)
+	digits := getDigits(arrMax(arr))
+	for k := 0; k < digits; k++ {
+		for _, v := range arr {
+			index := v / int(math.Pow10(k)) % 10
+			temp[index] = append(temp[index], v)
+		}
+		j := 0
+		for _, integers := range temp {
+			for _, integer := range integers {
+				arr[j] = integer
+				j++
+			}
+		}
+		clear(temp)
+	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 func main() {
 	//arr := [...]int{5, 2, 1, 3, 4, 7, 8, 6, 9, 0, 11, 12, 10}
-	arr := [...]int{0, 0, 3, 3, -5, -5, -8, -8, 1, 2, 3, 8, 4, 4, 9, 5, -5, 6, 6, 7, 2, 1}
+	//arr := [...]int{0, 0, 3, 3, -5, -5, -8, -8, 1, 2, 3, 8, 4, 4, 9, 5, -5, 6, 6, 7, 2, 1}
+	arr := [...]int{285, 33, 176, 684, 842, 551, 631, 408, 60, 74, 448}
 	nums := arr[:]
 	printArr(nums)
 	//BubbleSort(nums)
@@ -12,6 +13,7 @@ func main() {
 	//QuickSort(nums)
 	//MergeSort(nums)
 	//HeapSort(nums)
-	CountSort(nums)
+	//CountSort(nums)
+	RadixSort(nums)
 	printArr(nums)
 }
